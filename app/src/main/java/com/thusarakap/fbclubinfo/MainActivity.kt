af43b6f23602f9e648a7851614982404e6f8aa0e
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-04-17 01:46:25 +0530

    Update MainActivity.kt

--- app/src/main/java/com/thusarakap/fbclubinfo/MainActivity.kt
+++ app/src/main/java/com/thusarakap/fbclubinfo/MainActivity.kt
@@ -7,12 +7,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.thusarakap.fbclubinfo.database.DatabaseIO
 import com.thusarakap.fbclubinfo.ui.theme.FbClubInfoTheme
+import com.thusarakap.fbclubinfo.searchbyleague.SearchByLeagueUI
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        DatabaseIO.initialize(this)
 
         setContent {
             FbClubInfoTheme {
@@ -20,10 +23,12 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
 
                 // Define your NavHost
-                NavHost(navController = navController, startDestination = MainMenu.Screen1Route) {
-                    composable(MainMenu.Screen1Route) { MainMenu(navController) }
+                NavHost(navController = navController, startDestination = MainMenu.MainMenuRoute) {
+                    composable(MainMenu.MainMenuRoute) { MainMenu(navController) }
+                    composable(MainMenu.SearchByLeagueUIRoute) { SearchByLeagueUI(navController) }
                 }
             }
         }
     }
 }
+
