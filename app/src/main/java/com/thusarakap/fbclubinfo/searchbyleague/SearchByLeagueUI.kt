79c720fa4482a11d235e81e9305e286919831bd2
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-04-17 01:46:36 +0530

    Create SearchByLeagueUI.kt

---
+++ app/src/main/java/com/thusarakap/fbclubinfo/searchbyleague/SearchByLeagueUI.kt
@@ -0,0 +1,22 @@
+package com.thusarakap.fbclubinfo.searchbyleague
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+
+@Composable
+fun SearchByLeagueUI(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Search by League")
+    }
+}
