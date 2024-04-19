98cdf1d697c672a89bfbff87ba327626e3f2ae96
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-04-19 23:36:53 +0530

    Update AppDatabase.kt

--- app/src/main/java/com/thusarakap/fbclubinfo/database/AppDatabase.kt
+++ app/src/main/java/com/thusarakap/fbclubinfo/database/AppDatabase.kt
@@ -3,7 +3,9 @@ package com.thusarakap.fbclubinfo.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [League::class], version = 2)
+
+@Database(entities = [League::class, Club::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun leagueDao(): LeagueDao
+    abstract fun clubDao(): ClubDao // Add this line
 }
