c7b3a2c3a28d48283c85e33da1d9ceb3e051b5c4
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-06-05 21:04:58 +0530

    Update LogoLoader.kt

--- app/src/main/java/com/thusarakap/fbclubinfo/searchforclubs/LogoLoader.kt
+++ app/src/main/java/com/thusarakap/fbclubinfo/searchforclubs/LogoLoader.kt
@@ -5,16 +5,25 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.net.URL
 
+// Load image bitmap from URL
 suspend fun loadImageFromURL(url: String) = withContext(Dispatchers.IO) {
     try {
+        // Creating a URL object from the URL string
         val url = URL(url)
+        // Open connection to the URL
         val connection = url.openConnection()
+        // Allow input streams
         connection.doInput = true
+        // Connect to the URL
         connection.connect()
+        // Get the input stream from the connection
         val input = connection.getInputStream()
+        // Convert the input stream into a bitmap using BitmapFactory
         BitmapFactory.decodeStream(input)
     } catch (e: Exception) {
+        // Exception handling
         e.printStackTrace()
+        // Return null if an error occurs
         null
     }
 }
