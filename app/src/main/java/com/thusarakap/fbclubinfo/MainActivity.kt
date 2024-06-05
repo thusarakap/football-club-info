1ac89bc7b1b3e74c8149368949036d552b970ff0
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-06-05 21:05:03 +0530

    Update MainActivity.kt

--- app/src/main/java/com/thusarakap/fbclubinfo/MainActivity.kt
+++ app/src/main/java/com/thusarakap/fbclubinfo/MainActivity.kt
@@ -13,26 +13,31 @@ import com.thusarakap.fbclubinfo.searchbyleague.SearchByLeagueUI
 import com.thusarakap.fbclubinfo.searchforclubs.SearchForClubsUI
 import com.thusarakap.fbclubinfo.webjerseysearch.WebJerseySearchUI
 
+// MainActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        // Initialize the database
         DatabaseIO.initialize(this)
 
         setContent {
             FbClubInfoTheme {
-                // Create a NavController
+                // NavController
                 val navController = rememberNavController()
 
-                // Define your NavHost
+                // Define your NavHost with composable destinations
                 NavHost(navController = navController, startDestination = MainMenu.MainMenuRoute) {
+                    // Main menu screen
                     composable(MainMenu.MainMenuRoute) { MainMenu(navController) }
+                    // Search clubs by league screen
                     composable(MainMenu.SearchByLeagueUIRoute) { SearchByLeagueUI(navController) }
+                    // Search clubs screen
                     composable(MainMenu.SearchForClubsUIRoute) { SearchForClubsUI(navController) }
+                    // Search jerseys online screen
                     composable(MainMenu.WebJerseySearchUIRoute) { WebJerseySearchUI(navController) }
                 }
             }
         }
     }
 }
-
